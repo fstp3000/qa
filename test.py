6e64453af17b267e92e28cffc291aaaa8bae6a7d
fstp3000 <r06921064@ntu.edu.tw>
2019-05-28 16:35:52 +0800

    001

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import requests
-#from bs4 import BeautifulSoup
 import json
 
 url_list=['https://docs.google.com/forms/d/1xPSu0lT2Mr1XGSpVRlvk6DNQbeEySLOn3Kuxq0S-pv4/viewform?fbclid=IwAR3fUuFlZWmCWq1s7jVGLj6VSvSV3pp00_DtGRTc1_xXFFEW9xL7EqSKHQM&edit_requested=true#responses',
@@ -12,13 +11,20 @@ for url in url_list:
 	start = res.find('FB_PUBLIC_LOAD_DATA_')
 	end = res.find('</script>', start)
 	res = res[start+23:end-1]
-	#print(res)
 	res = json.loads(res)[1]
 	print('topic\n',res[0])
 	print('#####################################')
 	res = res[1]
 	for i in res:
-		print(i)
+		if i[-1] == 6:
+			print('specification\n',i[2])
+		elif i[3] == 2 or i[3]==4 or i[3] == 5:
+			print('question',i[1])
+			for answer in i[4][0][1]:
+				print(answer[0])
+			#print('answer',i[4][0][1])
+		else:
+			print(i)
 		print('--------------------------------------------------------------------------------------------')
 	print('=================================================================================================')
 	print('=================================================================================================')
