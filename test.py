b009d4a9b514221d86a44151fe26bb43a712b07b
Fstp <fstp@Fstpde-MacBook-Pro.local>
2019-05-29 16:04:11 +0800

    011

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 #coding=utf-8
 import requests
 import json
+import csv
 import pprint
-url_list=['https://docs.google.com/forms/d/1xPSu0lT2Mr1XGSpVRlvk6DNQbeEySLOn3Kuxq0S-pv4/viewform?fbclid=IwAR3fUuFlZWmCWq1s7jVGLj6VSvSV3pp00_DtGRTc1_xXFFEW9xL7EqSKHQM&edit_requested=true#responses','https://docs.google.com/forms/d/e/1FAIpQLSfKeNxWwzaEBEykLBivIBjkEZEbnJPaB65C-psY2CaVicrReA/viewform','https://docs.google.com/forms/d/e/1FAIpQLSfKeNxWwzaEBEykLBivIBjkEZEbnJPaB65C-psY2CaVicrReA/viewform','https://docs.google.com/forms/d/1xPSu0lT2Mr1XGSpVRlvk6DNQbeEySLOn3Kuxq0S-pv4/viewform?fbclid=IwAR3fUuFlZWmCWq1s7jVGLj6VSvSV3pp00_DtGRTc1_xXFFEW9xL7EqSKHQM&edit_requested=true#responses','https://docs.google.com/forms/d/e/1FAIpQLSdQICeYZ30apILh2VknzPqemIkzjb2kp_bv1LlVDUIRCMUUDg/viewform','https://docs.google.com/forms/d/e/1FAIpQLSeOkfOimyjzjQ_wHUb6GDVxv9NlzhBZPKxRHlFeJBe1iIGGgQ/viewform?fbclid=IwAR2qvgvotRONvox_9MNls_H5Fy2n8Q8P33HnphUUzd2DsLp4A-BgZtMROok','https://docs.google.com/forms/d/e/1FAIpQLSf7mx78hpk8BfDi_nrBeZ9uwynDC1RdIUzcymIoNXeyEiEVsA/viewform','https://docs.google.com/forms/d/e/1FAIpQLSfxZy5eNrgSJouLEti48QgpJnnh4eDs7m13H6gV1y_9g1fBEQ/viewform','https://docs.google.com/forms/d/e/1FAIpQLScwnJzVG25ZkHd03NPGyHyqx_FBxyYXJLNR4KSti817lmvUQQ/viewform','https://docs.google.com/forms/d/e/1FAIpQLSfqSBIUbn-1OX3ErlNvyBkNnckj0nmo-waBPSoTcKJqBvMAoQ/viewform','https://docs.google.com/forms/d/e/1FAIpQLSep4L-pfPppimxr2pS6QxSftitIa3FPCfKrXqKJyQ3ZDZmPtQ/viewform']
-
+with open('q.csv', newline='') as f:
+    url_list = csv.reader(f, delimiter=',')
+    url_list = list(url_list)
+#url_list=[url_list[2]]
+check_list = []
+check = []
+is_packqas = False
 for url in url_list:
+    check.append(url[0])
     data={"題目":"None","標題":"未命名的標題","標題描述": "無描述"}
-    res = requests.get(url).text
+    res = requests.get(url[1]).text
     topic_start = res.find('<meta itemprop="name" content="')
     topic_end = res.find('">', topic_start)
     start = res.find('FB_PUBLIC_LOAD_DATA_')
@@ -68,7 +75,43 @@ for url in url_list:
             ans["可選答案"]=uni_ans
             ans["答案"]="None"
             qas[question]=ans
+            is_packqas=False
             print('\n-----------------------------------------------------')
+        elif i[3] == 7:
+            try:
+                print(i)
+                if i[4] is None:
+                    continue
+                try:
+                    point = i[4][3][3][0].find('.')
+                except AttributeError:
+                    print(i[1])
+                if point != -1:
+                    question=i[4][3][3][0][i[4][3][3][0].find('.')+1:].replace('\t',"").replace('\n',"").replace(' ',"").replace('  ',"")
+                else:
+                    print(i[4][3][3][0])
+                    question=i[4][3][3][0].replace('\t',"").replace('\n',"")
+                print('Question:',question)
+                print('Answers:')
+                uni_ans=[]
+                ans={}
+                print(i[4][3][1])
+                for index,answer in enumerate(i[4][3][1]):
+                    #if answer[0]=="":
+                    #    break
+                    print(index,end='')
+                    print(':',end='')
+                    print(answer[0],end='')
+                    print('  ',end='')
+                    #print('answer',i[4][0][1])
+                    uni_ans.append(answer[0])
+                ans["可選答案"]=uni_ans
+                ans["答案"]="None"
+                qas[question]=ans
+                is_packqas=False
+                print('\n-----------------------------------------------------')
+            except:
+                continue
         elif i[3] == 8:
             id = 0
             if qas != {}:
@@ -93,12 +136,36 @@ for url in url_list:
             #assert (i[3] != 11)
             print(i)
             print('-----------------------------------------------------')
+    if is_packqas==False:
+        is_packqas=True
+        id = 0
+        if qas != {}:
+            for key, _ in qas.items():
+                if "性別" in key or "年齡" in key or "職業" in key:
+                    id += 1
+            if id> 2:
+                data["個資"]= qas
+            else:
+                group_qa['問題']=qas
+                set_qa.append(group_qa)
+            qas={}
+            group_qa = {"題組名稱":i[1]}
+            print('group question name:', i[1])
+            print('*****************************************************')
+        else:
+            qas={}
+            group_qa = {"題組名稱":i[1]}
+
     if set_qa != []:
         data["題組"]=set_qa
         set_qa=[]
         print(data)
-        with open(title+'.json', 'w', encoding='utf-8') as outfile:
+        with open('./qa/'+title+'.json', 'w', encoding='utf-8') as outfile:
             json.dump(data, outfile, ensure_ascii=False)
+        check.append('ok')
         print('=====================================================================')
         print('=====================================================================')
         print('=====================================================================')
+    check_list.append(check)
+    check = []
+pprint.pprint(check_list)
